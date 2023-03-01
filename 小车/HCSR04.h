d8f04182e167d8bd61fd709e80a9bc17a0bcf884
Mohan-HQy <99205419+Mohan-HQy@users.noreply.github.com>
2023-03-01 13:11:14 +0800

    Delete 小车 directory

--- 小车/HCSR04.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HCSR04_H__
-#define __HCSR04_H__
-
-    void Timer0Init(void);		 //@11.0592MHz
-	void ultra_init(void);       //³¬Éù²¨³õÊ¼ºÅ
-	int get_distance(void);      //²â¾à
-
-#endif
